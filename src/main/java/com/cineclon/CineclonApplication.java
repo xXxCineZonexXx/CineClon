cf6443bb6db5b7398fc93c4d5c754134a98b2e3d
Emerson-Lezama-Vera <emerson_00_35@hotmail.com>
2026-05-19 13:48:36 -0500

    Primer commit

---
+++ src/main/java/com/cineclon/CineclonApplication.java
@@ -0,0 +1,13 @@
+package com.cineclon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineclonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineclonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineclon/CineclonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineclonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
